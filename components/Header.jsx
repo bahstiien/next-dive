39091cbfce215c37d8de681c3cfe3073e962a8c9
bahstiien <bastien.lecalvez@gmail.com>
2021-12-02 18:18:20 +0100

    add auth

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,20 +10,25 @@ const Header = (props) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="flex">
-        <ul className="flex flex-row font-round">
-          <li className="pl-4 ">
-            <Link href="/"> Accueil </Link>
-          </li>
-          <li className="pl-4 ">
-            <Link href="/registration"> Inscription </Link>
-          </li>
-          <li className="pl-4 ">
-            <Link href="/clubs"> Nos clubs partenaires </Link>
-          </li>
-          <li className="pl-4 ">
-            <Link href="/contact"> Contact </Link>
-          </li>
-        </ul>
+        <nav>
+          <ul className="flex flex-row font-round">
+            <li className="pl-4 ">
+              <Link href="/"> Accueil </Link>
+            </li>
+            <li className="pl-4 ">
+              <Link href="/registration"> Inscription </Link>
+            </li>
+            <li className="pl-4 ">
+              <Link href="/clubs"> Nos clubs partenaires </Link>
+            </li>
+            <li className="pl-4 ">
+              <Link href="/contact"> Contact </Link>
+            </li>
+          </ul>
+
+          <button> Connexion </button>
+          <button>Inscription</button>
+        </nav>
         <div className="flex-end ml-24">
           <AccountCircleIcon fontSize="large" />
         </div>
